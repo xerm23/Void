28947092e574841936cb6ea8aea5005f8683aaac
xerm23 <m.radeljic43@gmail.com>
2021-01-27 20:51:26 +0100

    Photon Setup
    Made first scene possible for 2 players to join room and the first player becomes the master of the room. After both of them are ready master can start the game. The second scene is in early stage.

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,297 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ExitGames.Client.Photon;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+    public InputField PlayerNameInput;
+    public Button playButton;
+
+    [Header("Join Random Room Panel")]
+    public GameObject JoinRandomRoomPanel;
+
+    [Header("Inside Room Panel")]
+    public GameObject InsideRoomPanel;
+
+    public Button StartGameButton;
+    public GameObject PlayerListEntryPrefab;
+
+    private Dictionary<string, RoomInfo> cachedRoomList;
+    private Dictionary<string, GameObject> roomListEntries;
+    private Dictionary<int, GameObject> playerListEntries;
+
+    public const string PLAYER_READY = "IsPlayerReady";
+
+
+    private const string PlayerPrefsNameKey = "PlayerName";
+
+    #region UNITY
+
+    public void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        cachedRoomList = new Dictionary<string, RoomInfo>();
+        roomListEntries = new Dictionary<string, GameObject>();
+
+        PlayerNameInput.text = PlayerPrefs.GetString(PlayerPrefsNameKey);
+        changeButtonState();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+    }
+    #endregion
+
+    #region PUN CALLBACKS
+
+    public override void OnConnectedToMaster()
+    {
+        //this.SetActivePanel(SelectionPanel.name);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        string roomName = "Room " + Random.Range(1000, 10000);
+
+        RoomOptions options = new RoomOptions { MaxPlayers = 2 };
+
+        PhotonNetwork.CreateRoom(roomName, options, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        // joining (or entering) a room invalidates any cached lobby room list (even if LeaveLobby was not called due to just joining a room)
+        cachedRoomList.Clear();
+
+
+        SetActivePanel(InsideRoomPanel.name);
+
+        if (playerListEntries == null)
+        {
+            playerListEntries = new Dictionary<int, GameObject>();
+        }
+
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            GameObject entry = Instantiate(PlayerListEntryPrefab);
+            entry.transform.SetParent(InsideRoomPanel.transform);
+            entry.transform.localScale = Vector3.one;
+            entry.GetComponent<PlayerListEntry>().Initialize(p.ActorNumber, p.NickName);
+
+            object isPlayerReady;
+            if (p.CustomProperties.TryGetValue(PLAYER_READY, out isPlayerReady))
+            {
+                entry.GetComponent<PlayerListEntry>().SetPlayerReady((bool)isPlayerReady);
+            }
+
+            playerListEntries.Add(p.ActorNumber, entry);
+        }
+
+        StartGameButton.gameObject.SetActive(CheckPlayersReady());
+
+    }
+
+    public override void OnLeftRoom()
+    {
+        InsideRoomPanel.SetActive(false);
+
+        foreach (GameObject entry in playerListEntries.Values)
+        {
+            Destroy(entry.gameObject);
+        }
+
+        playerListEntries.Clear();
+        playerListEntries = null;
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        GameObject entry = Instantiate(PlayerListEntryPrefab);
+        entry.transform.SetParent(InsideRoomPanel.transform);
+        entry.transform.localScale = Vector3.one;
+        entry.GetComponent<PlayerListEntry>().Initialize(newPlayer.ActorNumber, newPlayer.NickName);
+
+        playerListEntries.Add(newPlayer.ActorNumber, entry);
+
+        StartGameButton.gameObject.SetActive(CheckPlayersReady());
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Destroy(playerListEntries[otherPlayer.ActorNumber].gameObject);
+        playerListEntries.Remove(otherPlayer.ActorNumber);
+
+        StartGameButton.gameObject.SetActive(CheckPlayersReady());
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        if (PhotonNetwork.LocalPlayer.ActorNumber == newMasterClient.ActorNumber)
+        {
+            StartGameButton.gameObject.SetActive(CheckPlayersReady());
+        }
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        if (playerListEntries == null)
+        {
+            playerListEntries = new Dictionary<int, GameObject>();
+        }
+
+        GameObject entry;
+        if (playerListEntries.TryGetValue(targetPlayer.ActorNumber, out entry))
+        {
+            object isPlayerReady;
+            if (changedProps.TryGetValue(PLAYER_READY, out isPlayerReady))
+            {
+                entry.GetComponent<PlayerListEntry>().SetPlayerReady((bool)isPlayerReady);
+            }
+        }
+
+        StartGameButton.gameObject.SetActive(CheckPlayersReady());
+    }
+
+    #endregion
+
+    #region UI CALLBACKS
+
+    public void OnBackButtonClicked()
+    {
+        if (PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+
+
+    }
+
+    public void OnJoinRandomRoomButtonClicked()
+    {
+        SetActivePanel(JoinRandomRoomPanel.name);
+
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public void OnLeaveGameButtonClicked()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public void OnLoginButtonClicked()
+    {
+        string playerName = PlayerNameInput.text;
+        SetActivePanel(JoinRandomRoomPanel.name);
+
+        if (!playerName.Equals(""))
+        {
+            PhotonNetwork.LocalPlayer.NickName = playerName;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            Debug.LogError("Player Name is invalid.");
+        }
+    }
+
+
+    public void OnStartGameButtonClicked()
+    {
+        PhotonNetwork.CurrentRoom.IsOpen = false;
+        PhotonNetwork.CurrentRoom.IsVisible = false;
+
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+    #endregion
+
+    private bool CheckPlayersReady()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return false;
+        }
+        if (PhotonNetwork.PlayerList.Length < PhotonNetwork.CurrentRoom.MaxPlayers)
+        {
+            return false;
+        }
+
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            object isPlayerReady;
+            if (p.CustomProperties.TryGetValue(PLAYER_READY, out isPlayerReady))
+            {
+                if (!(bool)isPlayerReady)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void LocalPlayerPropertiesUpdated()
+    {
+        StartGameButton.gameObject.SetActive(CheckPlayersReady());
+    }
+
+    private void SetActivePanel(string activePanel)
+    {
+
+        JoinRandomRoomPanel.SetActive(activePanel.Equals(JoinRandomRoomPanel.name));
+        InsideRoomPanel.SetActive(activePanel.Equals(InsideRoomPanel.name));
+    }
+
+    private void UpdateCachedRoomList(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo info in roomList)
+        {
+            // Remove room from cached room list if it got closed, became invisible or was marked as removed
+            if (!info.IsOpen || !info.IsVisible || info.RemovedFromList)
+            {
+                if (cachedRoomList.ContainsKey(info.Name))
+                {
+                    cachedRoomList.Remove(info.Name);
+                }
+
+                continue;
+            }
+
+            // Update cached room info
+            if (cachedRoomList.ContainsKey(info.Name))
+            {
+                cachedRoomList[info.Name] = info;
+            }
+            // Add new room info to cache
+            else
+            {
+                cachedRoomList.Add(info.Name, info);
+            }
+        }
+    }
+
+    public void changeButtonState()
+    {
+        playButton.interactable = !string.IsNullOrEmpty(PlayerNameInput.text);
+    }
+
+
+    public void saveName()
+    {
+        string playerName = PlayerNameInput.text;
+        PlayerPrefs.SetString(PlayerPrefsNameKey, playerName);
+    }
+}
---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+
+class PlayerMovement : MonoBehaviour
+{
+    private float moveSpeed = 20f;
+    private float gridSize = 5f;
+    private enum Orientation
+    {
+        Horizontal,
+        Vertical
+    };
+    private Orientation gridOrientation = Orientation.Vertical;
+    private bool allowDiagonals = false;
+    private bool correctDiagonalSpeed = true;
+    private Vector2 input;
+    private bool isMoving = false;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private float t;
+    private float factor;
+
+    [SerializeField] private Transform mainCam;
+
+
+    public void Update()
+    {
+        if (!isMoving)
+        {
+            input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (!allowDiagonals)
+            {
+                if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+                {
+                    input.y = 0;
+                }
+                else
+                {
+                    input.x = 0;
+                }
+            }
+
+            if (input != Vector2.zero)
+            {
+                StartCoroutine(move(transform));
+            }
+        }
+    }
+
+    public IEnumerator move(Transform transform)
+    {
+        isMoving = true;
+        startPosition = transform.position;
+        t = 0;
+
+        if (gridOrientation == Orientation.Horizontal)
+        {
+            endPosition = new Vector3(startPosition.x + System.Math.Sign(input.x) * gridSize,
+                startPosition.y, startPosition.z + System.Math.Sign(input.y) * gridSize);
+        }
+        else
+        {
+            endPosition = new Vector3(startPosition.x + System.Math.Sign(input.x) * gridSize,
+                startPosition.y + System.Math.Sign(input.y) * gridSize * 2, startPosition.z);
+        }
+
+        if (allowDiagonals && correctDiagonalSpeed && input.x != 0 && input.y != 0)
+        {
+            factor = 0.7071f;
+        }
+        else
+        {
+            factor = 1f;
+        }
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * (moveSpeed / gridSize) * factor;
+            transform.position = Vector3.Lerp(startPosition, endPosition, t);
+            mainCam.position = Vector3.Lerp(mainCam.transform.position, new Vector3(endPosition.x, mainCam.position.y, mainCam.position.z), t/5);
+            yield return null;
+        }
+
+        isMoving = false;
+        yield return 0;
+    }
+}
