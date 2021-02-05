7d1edd2385d88d00c682da9264ba1ceed64d58d8
xerm23 <m.radeljic43@gmail.com>
2021-02-05 10:56:08 +0100

    Fix
    fix folders

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,155 @@
+﻿using Photon.Pun;	
+using Photon.Realtime;	
+using System.Collections;	
+using System.Collections.Generic;	
+using UnityEngine;	
+using UnityEngine.SceneManagement;	
+using UnityEngine.UI;	
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+
+    public static GameManager instance;
+
+    #region SerializeFields	
+    int spawnIndex = 0;
+    [SerializeField] private Transform[] spawnPoints = null;
+    [SerializeField] private GameObject playerPrefab = null;
+    [SerializeField] private GameObject endGamePanel = null;
+    [SerializeField] public Button bombButton = null;
+    [SerializeField] private Text winnerText = null;
+
+
+    private Vector3 mainCamPos;
+    [SerializeField] private float camVerticalSpeed = 3;
+    [SerializeField] private GameObject[] platforms;
+    [SerializeField] private GameObject fallTrigger;
+    [SerializeField] private GameObject bombPickup;
+    string platformsStr = "Platforms/";
+    #endregion
+
+
+    #region PrivateValues	
+
+    int topHeight = 20;
+    #endregion
+
+
+
+    #region Unity	
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        mainCamPos = Camera.main.transform.position;
+    }
+
+    void Start()
+    {
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoints[(int)PhotonNetwork.LocalPlayer.CustomProperties["spawnIndex"]].position, Quaternion.identity);
+        initializeRows();
+    }
+
+    private void Update()
+    {
+        if (!endGamePanel.activeSelf)
+            updateRows();
+    }
+    #endregion
+
+
+
+    #region PlatformCreation	
+
+    void initializeRows()
+    {
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+            for (int i = -2; i < 3; i++)
+            {
+                for (int j = -2; j < 3; j++)
+                {
+                    if (i == -2 || i == 2 && j == 0)
+                        PhotonNetwork.InstantiateRoomObject(platformsStr + platforms[2].name, new Vector3(i * 5, 0, 0), Quaternion.identity);
+                    else
+                    {
+                        int spawnObj = Random.Range(0, platforms.Length + platforms.Length / 5);
+                        if (spawnObj < platforms.Length)
+                            PhotonNetwork.InstantiateRoomObject(platformsStr + platforms[spawnObj].name, new Vector3(i * 5, j * 10, 0), Quaternion.identity);
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+    void updateRows()
+    {
+        mainCamPos += new Vector3(0, camVerticalSpeed * Time.deltaTime, 0);
+        Camera.main.transform.position = mainCamPos;
+
+        if (mainCamPos.y > topHeight - 20 && PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+            topHeight += 10;
+            for (int i = -2; i < 3; i++)
+            {
+                int spawnObj = Random.Range(0, platforms.Length + 1);
+                if (spawnObj < platforms.Length)
+                    PhotonNetwork.InstantiateRoomObject(platformsStr + platforms[spawnObj].name, new Vector3(i * 5 + (int)((mainCamPos.x) / 5) * 5, topHeight, 0), Quaternion.identity);
+                PhotonNetwork.InstantiateRoomObject(platformsStr + fallTrigger.name, new Vector3(i * 5 + (int)((mainCamPos.x) / 5) * 5, topHeight + 5, 0), Quaternion.identity);
+                //  int bombSp = Random.Range(0, 10);	
+                //  if (bombSp == 0)	
+                //  PhotonNetwork.InstantiateRoomObject(platformsStr + bombPickup.name, new Vector3(i * 5 + (int)((mainCamPos.x) / 5) * 5, topHeight + 2, 0), Quaternion.identity);	
+
+            }
+
+        }
+    }
+
+
+    #endregion
+
+
+
+    #region public-functions-singleton	
+
+    public void showGameOver(string winnerName)
+    {
+        endGamePanel.SetActive(true);
+        winnerText.text = winnerName + " has won the game!";
+    }
+    public void showPlayerLeft()
+    {
+        endGamePanel.SetActive(true);
+        winnerText.text = "Opponent left the game.";
+    }
+
+
+    #endregion
+
+    #region UI-Callbacks	
+
+    public void onBtnLeaveRoomClicked()
+    {
+        PhotonNetwork.Disconnect();
+
+    }
+    #endregion
+
+
+    #region PUNCallbacks	
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    #endregion
+
+}
---
+++ Assets/_Scripts/PlayerListEntry.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+using Photon.Pun;
+
+public class PlayerListEntry : MonoBehaviour
+{
+    [Header("UI References")]
+    public Text PlayerNameText;
+
+    public Image PlayerColorImage;
+    public Button PlayerReadyButton;
+    public Image PlayerReadyImage;
+
+    private int ownerId;
+    private bool isPlayerReady;
+
+    #region UNITY	
+
+    public void OnEnable()
+    {
+        PlayerNumbering.OnPlayerNumberingChanged += OnPlayerNumberingChanged;
+    }
+
+    public void Start()
+    {
+        if (PhotonNetwork.LocalPlayer.ActorNumber != ownerId)
+        {
+            PlayerReadyButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            Hashtable initialProps = new Hashtable() { { MainMenu.PLAYER_READY, isPlayerReady } };
+            PhotonNetwork.LocalPlayer.SetCustomProperties(initialProps);
+            PhotonNetwork.LocalPlayer.SetScore(0);
+
+            PlayerReadyButton.onClick.AddListener(() =>
+            {
+                isPlayerReady = !isPlayerReady;
+                SetPlayerReady(isPlayerReady);
+
+                Hashtable props = new Hashtable() { { MainMenu.PLAYER_READY, isPlayerReady } };
+                PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+
+                if (PhotonNetwork.IsMasterClient)
+                {
+                    FindObjectOfType<MainMenu>().LocalPlayerPropertiesUpdated();
+                }
+            });
+        }
+    }
+
+    public void OnDisable()
+    {
+        PlayerNumbering.OnPlayerNumberingChanged -= OnPlayerNumberingChanged;
+    }
+
+    #endregion
+
+    public void Initialize(int playerId, string playerName, Sprite spr)
+    {
+        ownerId = playerId;
+        PlayerNameText.text = playerName;
+        PlayerColorImage.sprite = spr;
+    }
+
+    private void OnPlayerNumberingChanged()
+    {
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            if (p.ActorNumber == ownerId)
+            {
+                // PlayerColorImage.color = MainMenu.GetColor(p.GetPlayerNumber());	
+                //change playercolorImage here	
+            }
+        }
+    }
+
+    public void SetPlayerReady(bool playerReady)
+    {
+        PlayerReadyButton.GetComponentInChildren<Text>().text = playerReady ? "Ready!" : "Ready?";
+        PlayerReadyImage.enabled = playerReady;
+    }
+}
